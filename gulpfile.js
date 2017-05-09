b86c1c9c75f81e6a86e011b0ecf2e06499d8f550
Relei <690708337@qq.com>
2017-05-09 22:30:01 +0800

    修改ejs
    添加index list页面

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var rename = require('gulp-rename');
 
 gulp.task('sass', function () {
 	return gulp.src('./source/sass/**/import.scss')
-		.pipe(sass())
+		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(rename(function (path) {
+		    path.basename = "main";
+		}))
 		.pipe(gulp.dest('./dist/css/'));
 });
--- service.js
+++ service.js
@@ -8,8 +8,17 @@ app.engine('ejs', engine);
 app.set('views',__dirname + '/source/view');
 app.set('view engine', 'ejs');
 
+//静态文件
+app.use(express.static('dist'));
+
 app.get('/',function(req,res,next){
   res.render('index', { title: '首页'});
 });
 
-app.listen(3000);
+app.get('/list',function(req,res,next){
+  res.render('list', { title: '列表页'});
+});
+
+app.listen(3000, function() {
+	console.log('server start: http://localhost:3000/')
+});
